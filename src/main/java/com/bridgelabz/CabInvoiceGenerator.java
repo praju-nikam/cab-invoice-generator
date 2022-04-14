de353d47d92253f46a1c26e90a99335e6faa02d4
praju <sawantprajakta84@gmail.com>
2022-04-14 14:14:59 +0530

    Welcome Cab Invoice Service

---
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -0,0 +1,44 @@
+package com.bridgelabz;
+public class CabInvoiceGenerator {
+    RideRepository rideRepository = new RideRepository();
+    private final int COST_PER_KM = 10;
+    private final int COST_PER_MINUTE = 1;
+    public int MIN_FARE = 5;
+    double totalFare;
+
+    public double calculateFare(double distance, int time) {
+
+        totalFare = COST_PER_KM * distance + COST_PER_MINUTE * time;
+        if(totalFare < MIN_FARE)
+            totalFare = MIN_FARE;
+        return totalFare;
+    }
+
+    public double calculateFare(Rides[] rides) {
+        double totalFare = 0.0;
+        for (Rides ride : rides)
+            totalFare = totalFare + this.calculateFare(ride.distance, ride.time);
+
+        return totalFare;
+    }
+
+    public int numberOfRides(Rides[] rides) {
+        return rides.length;
+    }
+
+    public double calculateAverageFarePerRide(Rides[] rides) {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double totalFare = cabInvoiceGenerator.calculateFare(rides);
+        double numberOfRides = rides.length;
+        double averageFare = totalFare/numberOfRides;
+        return averageFare;
+    }
+
+    public double getRidesDetails(String userID) {
+        return this.calculateFare(rideRepository.getRides(userID));
+    }
+
+    public void addRides(String userID, Rides[] rides) {
+        rideRepository.addRides(userID, rides);
+    }
+}
---
+++ src/main/java/com/bridgelabz/PrimiumRides.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+public class PrimiumRides {
+
+    public static final PrimiumRides PREMIUM = null;
+    public static PrimiumRides Normal;
+
+    private final double costperkm;
+    private final double costpermin;
+    private final double minfare;
+
+    PrimiumRides(double costperkm, double costpermin, double minfare){
+        this.costperkm=costperkm;
+        this.costpermin=costpermin;
+        this.minfare=minfare;
+    }
+
+    public double calcCostOfCabRide(Rides ride) {
+        double rideCost = ride.distance * costperkm + ride.time * costpermin;
+        return rideCost;
+    }
+}
