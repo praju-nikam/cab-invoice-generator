23af664dea06310683d689f5d97adfe77762875e
praju <sawantprajakta84@gmail.com>
2022-04-14 14:24:27 +0530

    Added Calculate Multiple Rides

---
+++ src/main/java/com/bridgelabz/Rides.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class Rides {
+    public double distance;
+    public int time;
+    public PrimiumRides rides;
+
+    public Rides(double distance, int time, PrimiumRides rides) {
+        this.distance = distance;
+        this.time = time;
+        this.rides=rides;
+    }
+}
